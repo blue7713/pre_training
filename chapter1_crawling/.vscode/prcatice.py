c2c3faf7223ff94f3cdd17d26f26a98aa3cbca49
Ryu hyeon seok <gustjr1058@naver.com>
2023-12-22 15:06:25 +0900

    test_1

---
+++ chapter1_crawling/.vscode/prcatice.py
@@ -0,0 +1,16 @@
+print("hello World")
+
+print(15 / 7) # 나누기
+print(15 // 3) # 몫
+print(7 % 3) # 나머지
+
+print(2 ** 3) # 2의 3승
+
+digit = 1
+name = "name" # string
+num = True # boolean
+
+print(34 == 100)
+print(15 > 7)
+print(3 <= 10)
+print(3 != 100)
